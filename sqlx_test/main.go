e1c1bf3be841920237c35d27c44d81d526520b76
kip2 <tqa1no4@gmail.com>
2024-10-01 20:49:07 +0900

    Add comment

--- sqlx_test/main.go
+++ sqlx_test/main.go
@@ -53,11 +53,13 @@ func createDBConnection() *sqlx.DB {
 .envファイルから特定のキーに紐づく値を取得する関数
 */
 func loadEnv(key string) string {
+	// .envに定義した環境変数をロード
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
+	// 環境変数から値を取得
 	env := os.Getenv(key)
 
 	return env
