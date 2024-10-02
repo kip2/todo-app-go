089b2b47dc205698793fb54d0ee273448c781c74
kip2 <tqa1no4@gmail.com>
2024-10-02 20:46:37 +0900

    Add process to retrieve data from the database

--- todo-app-go/main.go
+++ todo-app-go/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "todoApp/db"
+import (
+	"fmt"
+	"time"
+	"todoApp/db"
+	errorpkg "todoApp/error"
+)
+
+type Todo struct {
+	ID        int        `db:"ID"`
+	Content   string     `db:"Content"`
+	Done      bool       `db:"Done"`
+	Until     *time.Time `db:"Until"`
+	CreatedAt time.Time  `db:"CreatedAt"`
+	UpdatedAt time.Time  `db:"UpdatedAt"`
+	DeletedAt *time.Time `db:"DeletedAt"`
+}
 
 func main() {
-	name := "David"
-	db.Insert(name)
+
+	envVar := "DATABASE"
+	db := db.CreateDBConnection(envVar)
+	defer db.Close()
+
+	var todo Todo
+	err := db.Get(&todo, "SELECT * FROM todos WHERE id=?", 1)
+	errorpkg.CheckError(err)
+
+	fmt.Printf("Todo: %+v\n", todo)
 }
